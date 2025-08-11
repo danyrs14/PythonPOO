9ce0d0a6be302eb35a63e9abf5212a83f2754528
danyrs14 <eminemrs14@gmail.com>
2025-08-10 21:07:58 -0600

    ejercicio de clases basicas con metodos

---
+++ pelicula.py
@@ -0,0 +1,28 @@
+"""
+Crea una clase Pelicula con atributos: nombre, director, duracion, clasificacion.
+Agrega un métodos reproducir() que imprima "Reproduciendo <nombre>".
+Crea una instancia, asigna los valores y llama al métodos.
+"""
+
+class Pelicula:
+    nombre = ""
+    director = ""
+    duracion = ""
+    clasificacion = ""
+
+    def reproducir(self):
+        #de esta manera se concatena los atributos en las clases
+        return "Reproduciendo: " + self.nombre
+
+name = str(input("Nombre de la Pelicula: "))
+director = str(input("Director de la pelicula: "))
+duracion = int(input("Duracion en min: "))
+clasificacion = str(input("Clasificacion de la pelicula: "))
+movile = Pelicula()
+movile.nombre = name
+movile.director = director
+movile.duracion = duracion
+movile.clasificacion = clasificacion
+print("Nombre: ", movile.nombre)
+print("Director: ", movile.director)
+print(movile.reproducir())
